8c39142363df98c3681f0375bc555c54d41966af
Denton Gentry <dgentry@tailscale.com>
2023-01-28 15:34:22 -0800

    Replace with recursive resolver.
    https://gist.github.com/timothyandrew/c5d13b5957f1323ea775705ff9374ff1
    
    Signed-off-by: Denton Gentry <dgentry@tailscale.com>

--- main.go
+++ main.go
@@ -2,114 +2,91 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"math/rand"
+	"os"
 
 	"github.com/miekg/dns"
 )
 
-func getResponse(requestMsg *dns.Msg) (*dns.Msg, error) {
-	responseMsg := new(dns.Msg)
-	if len(requestMsg.Question) == 0 {
-		return responseMsg, nil
+var (
+	ROOT_NAMESERVERS = []string{
+		"198.41.0.4", "199.9.14.201", "192.33.4.12", "199.7.91.13", "192.203.230.10",
+		"192.5.5.241", "192.112.36.4", "198.97.190.53", "192.36.148.17", "192.58.128.30",
+		"193.0.14.129", "199.7.83.42", "202.12.27.33",
 	}
-	question := requestMsg.Question[0]
-	dnsServer := "8.8.8.8:53"
+)
 
-	switch question.Qtype {
-	case dns.TypeAAAA:
-		answer, err := processTypeAAAA(dnsServer, question, requestMsg)
+func resolve(name string) ([]dns.RR, error) {
+	nameserver := ROOT_NAMESERVERS[rand.Intn(len(ROOT_NAMESERVERS))]
+	c := new(dns.Client)
+
+	for {
+		// Prepare a message asking for an A record (an IP address) for `name`
+		m := new(dns.Msg)
+		m.SetQuestion(dns.Fqdn(name), dns.TypeA)
+
+		// Send the DNS request to the IP in `nameserver`
+		fmt.Printf("Asking %s about %s\n", nameserver, name)
+		resp, _, err := c.Exchange(m, fmt.Sprintf("%s:53", nameserver))
 		if err != nil {
-			return responseMsg, err
+			return nil, err
 		}
-		responseMsg.Answer = append(responseMsg.Answer, *answer)
 
-	case dns.TypeA:
-		// no answer
+		if len(resp.Answer) > 0 {
+			// If an ANSWER SECTION exists and contains a CNAME, recurse
+			if cname, ok := resp.Answer[0].(*dns.CNAME); ok {
+				return resolve(cname.Target)
+			}
 
-	default:
-		answer, err := processOtherTypes(dnsServer, &question, requestMsg)
-		if err != nil {
-			return responseMsg, err
+			// If an ANSWER SECTION exists, we're done
+			return resp.Answer, nil
 		}
-		responseMsg.Answer = append(responseMsg.Answer, *answer)
+
+		// If the ADDITIONAL SECTION is empty and the AUTHORITY SECTION is not, resolve
+		// one of the names in the AUTHORITY SECTION and have that be the nameserver
+		if len(resp.Extra) == 0 && len(resp.Ns) != 0 {
+			ns := resp.Ns[0].(*dns.NS)
+			nsIP, err := resolve(ns.Ns)
+			if err != nil {
+				return nil, fmt.Errorf("break in the chain")
+			}
+			nameserver = nsIP[0].(*dns.A).A.String()
+		} else {
+			// If an ADDITIONAL SECTION exists, look in it for an A record for the
+			// next-level nameserver. If one doesn't exist, we have to error out
+			found := false
+			for _, rr := range resp.Extra {
+				record, ok := rr.(*dns.A)
+				if ok {
+					nameserver = record.A.String()
+					found = true
+					break
+				}
+			}
+			if !found {
+				return nil, fmt.Errorf("break in the chain")
+			}
+		}
+
+		// ... and recurse!
 	}
 
-	return responseMsg, nil
-}
-
-func processOtherTypes(dnsServer string, q *dns.Question, requestMsg *dns.Msg) (*dns.RR, error) {
-	queryMsg := new(dns.Msg)
-	requestMsg.CopyTo(queryMsg)
-	queryMsg.Question = []dns.Question{*q}
-
-	msg, err := lookup(dnsServer, queryMsg)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(msg.Answer) > 0 {
-		return &msg.Answer[0], nil
-	}
-	return nil, fmt.Errorf("not found")
-}
-
-func processTypeAAAA(dnsServer string, q dns.Question, requestMsg *dns.Msg) (*dns.RR, error) {
-	queryMsg := new(dns.Msg)
-	requestMsg.CopyTo(queryMsg)
-	queryMsg.Question = []dns.Question{q}
-	queryMsg.Question[0].Qtype = dns.TypeA
-
-	msg, err := lookup(dnsServer, queryMsg)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(msg.Answer) == 0 {
-		return nil, fmt.Errorf("not found")
-	}
-
-	// wrap IPv4 address for 4via6
-	a, ok := msg.Answer[0].(*dns.A)
-	if !ok {
-		return &msg.Answer[0], nil
-	}
-
-	via6 := "fd7a:115c:a1e0:b1a:0:fe:" + a.A.String()
-	rr, err := dns.NewRR(fmt.Sprintf("%s 120 IN AAAA %s", q.Name, via6))
-	if err != nil {
-		return nil, err
-	}
-
-	return &rr, nil
-}
-
-func lookup(server string, m *dns.Msg) (*dns.Msg, error) {
-	dnsClient := new(dns.Client)
-	dnsClient.Net = "udp"
-	response, _, err := dnsClient.Exchange(m, server)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
 }
 
 func main() {
-	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
-		switch r.Opcode {
-		case dns.OpcodeQuery:
-			m, err := getResponse(r)
-			if err != nil {
-				log.Printf("Failed lookup for %s with error: %s\n", r, err.Error())
-			}
-			m.SetReply(r)
-			w.WriteMsg(m)
-		}
-	})
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: dns <name>\n")
+		os.Exit(1)
+	}
 
-	server := &dns.Server{Addr: "100.127.188.88:53", Net: "udp"}
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("Failed to start server: %s\n ", err.Error())
+	name := os.Args[1]
+	answer, err := resolve(name)
+	if err == nil {
+		for _, record := range answer {
+			fmt.Println(record)
+		}
+	} else {
+		fmt.Fprintf(os.Stderr, "Failed to resolve %s\n", name)
+		os.Exit(1)
 	}
 }
